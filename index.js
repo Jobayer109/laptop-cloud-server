55d69a568d680444c04e4f1f2a3d66c13d046287
Nightowlam <nightowl3.00am@gmail.com>
2022-11-26 12:33:39 +0600

    Basic jwt applied

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const stripe = require("stripe")(
   "sk_test_51M6ARNB16mRcRBwtghZDWVXEnZ7buzkqGsONphwXADjLW0cCmhFuzf75i8DGnQK8UFkozJEwg6VsZAGGhI837rQT00bJX0iEf6"
@@ -20,6 +21,23 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  // console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, function (error, decoded) {
+    if (error) {
+      return res.status(403).send("Forbidden access");
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 const dbConnect = async () => {
   try {
     const categoriesCollection = client.db("laptop-cloud").collection("categories");
@@ -28,6 +46,18 @@ const dbConnect = async () => {
     const paymentsCollection = client.db("laptop-cloud").collection("payments");
     const productsCollection = client.db("laptop-cloud").collection("products");
     const usersCollection = client.db("laptop-cloud").collection("users");
+
+    // JWT api
+    app.get("/jwt", async (req, res) => {
+      const query = { email: req.query.email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "10d" });
+        res.send({ token });
+      } else {
+        res.status(403).send("Forbidden Access");
+      }
+    });
 
     // All categories api
     app.get("/categories", async (req, res) => {
@@ -51,8 +81,13 @@ const dbConnect = async () => {
     });
 
     // My orders api
-    app.get("/myOrders", async (req, res) => {
+    app.get("/myOrders", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const query = { email: req.query.email };
+
+      if (req.query.email !== decodedEmail) {
+        return res.status(403).send("Forbidden access");
+      }
       const orders = await bookingsCollection.find(query).toArray();
       res.send(orders);
     });
@@ -61,27 +96,6 @@ const dbConnect = async () => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await bookingsCollection.findOne(query);
-      res.send(result);
-    });
-
-    // Add product by seller api
-    app.post("/products", async (req, res) => {
-      const product = req.body;
-      const products = await productsCollection.insertOne(product);
-      res.send(products);
-    });
-
-    // Seller products api
-    app.get("/myProducts", async (req, res) => {
-      const query = { email: req.query.email };
-      const result = await productsCollection.find(query).toArray();
-      res.send(result);
-    });
-
-    // Delete seller's product.
-    app.delete("/myProducts/:id", async (req, res) => {
-      const query = { _id: ObjectId(req.params.id) };
-      const result = await productsCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -113,6 +127,27 @@ const dbConnect = async () => {
         },
       };
       const updateResult = await bookingsCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    // Add product by seller api
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const products = await productsCollection.insertOne(product);
+      res.send(products);
+    });
+
+    // Seller products api
+    app.get("/myProducts", async (req, res) => {
+      const query = { email: req.query.email };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Delete seller's product.
+    app.delete("/myProducts/:id", async (req, res) => {
+      const query = { _id: ObjectId(req.params.id) };
+      const result = await productsCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -154,6 +189,13 @@ const dbConnect = async () => {
       const query = { email: req.params.email };
       const user = await usersCollection.findOne(query);
       res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    // Seller verify API
+    app.get("/users/seller/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isSeller: user?.role === "seller" });
     });
   } finally {
   }
